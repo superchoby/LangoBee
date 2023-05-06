bebeebe2c5a1abf81a75ffac8bea74bd061f5a7d
Thomas Trinh <thomasqtrnh@gmail.com>
2023-05-06 19:06:40 -0400

    small models update

--- server/subscriptions/models.py
+++ server/subscriptions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Subscription(models.Model):
@@ -9,8 +10,8 @@ class Subscription(models.Model):
     ]
 
     subscription_plan = models.CharField(choices=SUBSCRIPTION_TYPES, max_length=max(len(sub_type[0]) for sub_type in SUBSCRIPTION_TYPES), null=True)
-    stripe_price_id = models.CharField(max_length=50)
+    stripe_price_id = models.CharField(max_length=50, null=True)
     stripe_subscription_id = models.CharField(max_length=100, null=True)
-    start_date = models.DateTimeField()
+    start_date = models.DateTimeField(default=timezone.now)
     end_date = models.DateTimeField(null=True, blank=True)
     
